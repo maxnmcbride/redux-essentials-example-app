bef911f55fc07dbc1d6501e0598aa00d4840b9f8
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-06-26 15:12:28 -0400

    finished reaction button

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,39 +2,43 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from './TimeAgo';
+import { ReactionButtons } from './ReactionButtons';
 
 export const SinglePostPage = ({ match }) => {
 
-  const { postId } = match.params;
+    const { postId } = match.params;
 
-  // component will re-render any time the value returned 
-  // from useSelector changes to a new reference.
-  const post = useSelector(state => 
-    state.posts.find(post => post.id === postId)
-  )
-
-  // if no post/postId, find() function will return undefined
-  // instead of an actual post object, error handling:
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
+    // component will re-render any time the value returned 
+    // from useSelector changes to a new reference.
+    const post = useSelector(state =>
+        state.posts.find(post => post.id === postId)
     )
-  }
 
-  return (
-    <section>
-      <article className="post">
-        <h2>{post.title}</h2>
-        <div>
-          <PostAuthor userId={post.user}/>
-        </div>
-        <p className="post-section">{post.content}</p>
-        <Link to={`/editPost/${post.id}`} className="button">
-          Edit Post
-        </Link>
-      </article>
-    </section>
-  )
+    // if no post/postId, find() function will return undefined
+    // instead of an actual post object, error handling:
+    if (!post) {
+        return (
+            <section>
+                <h2>Post not found!</h2>
+            </section>
+        )
+    }
+
+    return (
+        <section>
+            <article className="post">
+                <h2>{post.title}</h2>
+                <div>
+                    <PostAuthor userId={post.user} />
+                    <TimeAgo timestamp={post.date} />
+                </div>                
+                <p className="post-section">{post.content}</p>
+                <ReactionButtons post={post}/>
+                <Link to={`/editPost/${post.id}`} className="button">
+                    Edit Post
+                </Link>
+            </article>
+        </section>
+    )
 }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,17 +1,34 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns'
 
 const initialState = [
   {
     id: '1',
     title: 'First Post!',
     content: 'Hello!',
-    user: '0'
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    user: '0',
+    reactions: {
+      thumbsUp: 0,
+      hooray: 0,
+      heart: 0,
+      rocket: 0,
+      eyes: 0,
+    },
   },
   {
     id: '2',
     title: 'Second Post',
     content: 'More text',
-    user: '2'
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    user: '2',
+    reactions: {
+      thumbsUp: 0,
+      hooray: 0,
+      heart: 0,
+      rocket: 0,
+      eyes: 0,
+    },
   },
 ]
 
@@ -33,8 +50,22 @@ const postsSlice = createSlice({
             title,
             content,
             user: userId,
-          }
+            reactions: {
+              thumbsUp: 0,
+              hooray: 0,
+              heart: 0,
+              rocket: 0,
+              eyes: 0,
+            },
+          },
         }
+      },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
       }
     },
     postUpdated(state, action) {
@@ -50,6 +81,6 @@ const postsSlice = createSlice({
 
 // we then export the action creator and use it in our components to 
 // dispatch the action when the user clicks "save post" or "edit post":
-export const { postAdded, postUpdated } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer;
